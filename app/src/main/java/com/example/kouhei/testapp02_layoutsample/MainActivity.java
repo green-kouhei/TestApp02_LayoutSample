e914995b669054d7fc45001828218ea9dadffe2c
Your Name <you@example.com>
2016-04-18 18:53:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kouhei/testapp02_layoutsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kouhei.testapp02_layoutsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
